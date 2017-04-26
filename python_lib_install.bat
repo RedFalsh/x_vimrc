f9ad3322d4923f65f5dd6e9a60c1fffe60432b86
xiongbigboss <13693421942@163.com>
2017-04-26 17:19:06 +0800

    添加快捷键使用说明

---
+++ python_lib_install.bat
@@ -0,0 +1,8 @@
+python -m pip install numpy
+python -m pip install pandas
+python -m pip install matplotlib
+python -m pip install PyQt5
+python -m pip install xlrd
+python -m pip install xlwt
+python -m pip install configobj
+
